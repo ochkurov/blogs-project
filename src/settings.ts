ff4af61f44759181d6c02bb0649196f21dd75457
chik1313 <ochkurov96@gmail.com>
2024-12-23 14:14:27 +0100

    add vercel.json

--- src/settings.ts
+++ src/settings.ts
@@ -6,7 +6,7 @@ config()
 export const SETTINGS = {
     // все хардкодные значения должны быть здесь, для удобства их изменения
     PORT: process.env.PORT || 3003,
-    logger: process.env.NODE_ENV !== 'production'
+
     CREDENTIAL: {
       LOGIN: 'admin',
       PASSWORD: 'qwerty',
