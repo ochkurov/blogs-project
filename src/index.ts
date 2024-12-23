3eb1c69d48d3f03a2d45069b49e1bd2d1ed3b688
chik1313 <ochkurov96@gmail.com>
2024-12-23 14:04:44 +0100

    add vercel.json

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import {app} from './app'
 import {SETTINGS} from './settings'
 
 app.listen(SETTINGS.PORT, () => {
-    console.log('...server started in port ' + SETTINGS.PORT)
+    console.log('...server started in port !!' + SETTINGS.PORT)
 })
