c39563111aa7ad7ddc91c2dc542a9608e28bc4c1
chik1313 <ochkurov96@gmail.com>
2024-12-24 12:21:02 +0100

    add express validator

--- src/validation/field-validator.ts
+++ src/validation/field-validator.ts
@@ -2,8 +2,8 @@ import {body} from "express-validator";
 
 export const blogsBodyValidation = [
     body('name').isString().trim().notEmpty().isLength({min:1 , max:15}),
-    body('websiteUrl').isString().trim().notEmpty().matches(new RegExp('^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$')).isLength({min:1 , max:500}).withMessage('should be string'),
-    body('description').isString().trim().notEmpty().isLength({min:1 , max:100}).withMessage('should be string'),
+    body('websiteUrl').isString().trim().notEmpty().matches(new RegExp('^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$')).isLength({min:1 , max:100}).withMessage('should be string'),
+    body('description').isString().trim().notEmpty().isLength({min:1 , max:500}).withMessage('should be string'),
 ]
 
 export const postBodyValidation = [
