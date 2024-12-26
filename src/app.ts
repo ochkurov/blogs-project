60e76142c87d8957f13e461911b3fae33795b53d
chik1313 <ochkurov96@gmail.com>
2024-12-26 12:48:37 +0100

    change structure

--- src/app.ts
+++ src/app.ts
@@ -5,19 +5,18 @@ import {db} from "./db/db";
 import {blogsRouter} from "./blogs/blogs-router";
 import {postsRouter} from "./posts/post-router";
 
-export const app = express() // создать приложение
-app.use(express.json()) // создание свойств-объектов body и query во всех реквестах
-app.use(cors()) // разрешить любым фронтам делать запросы на наш бэк
+export const initApp = ()=>{
+    const app = express() // создать приложение
+    app.use(express.json()) // создание свойств-объектов body и query во всех реквестах
+    app.use(cors()) // разрешить любым фронтам делать запросы на наш бэк
 
-app.get('/', (req, res) => {
-    // эндпоинт, который будет показывать на верселе какая версия бэкэнда сейчас залита
-    res.status(200).json({version: '1.0'})
-})
-app.delete('/testing/all-data', (req, res) => {
-    db.posts = []
-    db.blogs
-    res.sendStatus(204)
-})
+    app.use(SETTINGS.PATH.BLOGS, blogsRouter)
+    app.use(SETTINGS.PATH.POSTS, postsRouter)
 
-app.use(SETTINGS.PATH.BLOGS, blogsRouter)
-app.use(SETTINGS.PATH.POSTS, postsRouter)
+    return app
+}
+
+
+
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
-import {app} from './app'
+import {initApp} from './app'
 import {SETTINGS} from './settings'
 import {runDb} from "./db/mongoDb";
+import {db} from "./db/db";
+
+const app = initApp()
+
+app.get('/', (req, res) => {
+    // эндпоинт, который будет показывать на верселе какая версия бэкэнда сейчас залита
+    res.status(200).json({version: '1.0'})
+})
+app.delete('/testing/all-data', (req, res) => {
+    db.posts = []
+    db.blogs
+    res.sendStatus(204)
+})
 
 const startApp = async () => {
 
