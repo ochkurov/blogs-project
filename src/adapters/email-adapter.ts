5da771e7301543fdcd61fca960d87307cdb180d2
chik1313 <ochkurov96@gmail.com>
2025-02-06 21:20:17 +0100

    add nodemailer

---
+++ src/adapters/email-adapter.ts
@@ -0,0 +1,26 @@
+import nodemailer from 'nodemailer'
+import {SETTINGS} from "../settings";
+
+
+
+export const emailSender = {
+   async send (email:string , subject: string ) {
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: "backenddeveloper409@gmail.com",
+                pass: SETTINGS.EMAIL_PASSWORD,
+            },
+        });
+        const info = await transporter.sendMail({
+            from: 'I BackendDeveloper',
+            to: email,
+            subject: subject,
+            html: "<h1>Thanks for your registration</h1>\n" +
+                " <p>To finish registration please follow the link below:\n" +
+                "     <a href='https://somesite.com/confirm-email?code=your_confirmation_code'>complete registration</a>\n" +
+                " </p>",
+        });
+        return info;
+    }
+}
