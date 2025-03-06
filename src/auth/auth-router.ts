38408b4fbf9d3e8791bdc9a82deb082c246f4393
chik1313 <ochkurov96@gmail.com>
2025-03-06 12:56:50 +0100

    add middleware to auth-router.ts

--- src/auth/auth-router.ts
+++ src/auth/auth-router.ts
@@ -5,14 +5,15 @@ import {errorsResultMiddleware} from "../middlewares/errorsResultMiddleware";
 import {confirmationCodeValidate, userCredentialsValidate} from "../users/middlewares/UserCredentialsValidate";
 import {refreshTokenMiddleware} from "./middlewares/RefreshTokenMiddleware";
 import {authBearerMiddleware} from "./middlewares/authBearerMiddleware";
+import {rateLimitMiddleware} from "./middlewares/rateLimitMiddleware";
 
 export const authRouter = Router()
 
-authRouter.post('/login' , authValidate , errorsResultMiddleware ,  authController.Login)
+authRouter.post('/login' ,rateLimitMiddleware ,authValidate , errorsResultMiddleware ,  authController.Login)
 authRouter.get('/me' ,authBearerMiddleware ,authController.Me)
 
-authRouter.post('/registration-confirmation' , confirmationCodeValidate , errorsResultMiddleware , authController.ConfirmationByCode)
-authRouter.post('/registration' , ...userCredentialsValidate , errorsResultMiddleware , authController.Registration)
-authRouter.post('/registration-email-resending' , userCredentialsValidate[1], errorsResultMiddleware  ,authController.RegistrationCodeResending)
+authRouter.post('/registration-confirmation' , rateLimitMiddleware ,confirmationCodeValidate , errorsResultMiddleware , authController.ConfirmationByCode)
+authRouter.post('/registration' , rateLimitMiddleware , ...userCredentialsValidate , errorsResultMiddleware , authController.Registration)
+authRouter.post('/registration-email-resending' , rateLimitMiddleware , userCredentialsValidate[1], errorsResultMiddleware  ,authController.RegistrationCodeResending)
 authRouter.post('/refresh-token' ,refreshTokenMiddleware ,authController.Refresh_Token)
 authRouter.post('/logout' , refreshTokenMiddleware , authController.logout)
