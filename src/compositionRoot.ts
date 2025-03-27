95b2d299d154c308eb3bfdf96a66fac833a9204e
Vladyslav <ochkurov96@gmail.com>
2025-03-27 21:40:41 +0100

    make some changes

--- src/compositionRoot.ts
+++ src/compositionRoot.ts
@@ -6,12 +6,18 @@ import {UsersRepository} from "./users/usersRepository";
 import {AuthBearerGuard} from "./auth/middlewares/authBearerMiddleware";
 import {RefreshTokenGuard} from "./auth/middlewares/RefreshTokenMiddleware";
 import {BlogsRepository} from "./blogs/blogsRepository";
+import {BlogsController} from "./blogs/blogsController";
+import {PostsService} from "./posts/posts-service";
+import {BlogsService} from "./blogs/blogs-service";
 
 
 const userRepository = new UsersRepository()
 const usersService = new UserService(userRepository)
 const blogsRepository = new BlogsRepository()
+const postsService = new PostsService()
 
+const blogsService = new BlogsService(blogsRepository)
+const blogsController = new BlogsController(blogsService , postsService)
 const userQueryRepository = new UsersQwRepository()
 const authService = new AuthService(userRepository, usersService)
 const authController = new AuthController(authService, userQueryRepository);
