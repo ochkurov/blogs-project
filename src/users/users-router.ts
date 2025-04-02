3e8cf70b91fb86ae39962daba4ec1400ad5a5160
chik1313 <ochkurov96@gmail.com>
2025-04-02 17:47:39 +0200

    add Dependecy injection

--- src/users/users-router.ts
+++ src/users/users-router.ts
@@ -10,3 +10,4 @@ export const usersRouter = Router();
 usersRouter.get('/' , authorizationMidleware , userController.getUsers.bind(userController))
 usersRouter.post('/' , authorizationMidleware , ...userCredentialsValidate , errorsResultMiddleware , userController.createUser.bind(userController))
 usersRouter.delete('/:id' , authorizationMidleware  , userController.deleteUser.bind(userController))
+s
