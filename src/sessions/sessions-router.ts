0c29ec8dc4f1954d3d5c3a6dc14939d36171190c
chik1313 <ochkurov96@gmail.com>
2025-04-03 21:20:38 +0200

    add Dependecy injection

--- src/sessions/sessions-router.ts
+++ src/sessions/sessions-router.ts
@@ -6,3 +6,4 @@ export const sessionRouter = Router();
 sessionRouter.get('/devices' , refreshTokenMiddleware.execute ,  sessionController.getAllDeviceSessions)
 sessionRouter.delete('/devices'  , refreshTokenMiddleware.execute , sessionController.terminateAllOtherSessions)
 sessionRouter.delete('/devices/:deviceId' , refreshTokenMiddleware.execute , sessionController.terminateSessionByDeviceId)
+k
