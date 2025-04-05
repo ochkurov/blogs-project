fcd461642efbbacfbbfa40de5b5510575340f8d7
chik1313 <ochkurov96@gmail.com>
2025-04-05 23:14:33 +0200

    add Dependecy injection

--- src/db/mongoDb.ts
+++ src/db/mongoDb.ts
@@ -2,6 +2,8 @@ import {SETTINGS} from "../settings";
 import {MongoClient} from "mongodb";
 import type {Collection} from 'mongodb'
 import {SessionType} from "../sessions/types/session-types";
+import mongoose from "mongoose";
+import {BlogDbType} from "../types/blog-types";
 
 
 export let postsCollection: Collection<any>
@@ -10,6 +12,15 @@ export let usersCollection: Collection<any>
 export let commentsCollection: Collection<any>
 export let deviceCollection: Collection<SessionType>
 export let countRequestsCollection: Collection<any>
+
+const BlogsSchema = new mongoose.Schema<BlogDbType>({
+    name: String,
+    description: String,
+    websiteUrl: String,
+    createdAt: String,
+    isMembership: Boolean
+})
+export const BlogsModel = mongoose.model(SETTINGS.DB_COLLECTION_NAME.BLOGS, BlogsSchema)
 
 export async function runDb(url: string): Promise<boolean> {
     let client = new MongoClient(url)
@@ -24,13 +35,17 @@ export async function runDb(url: string): Promise<boolean> {
 
 
     try {
-        await client.connect();
-        await db.command({ping: 1});
+        await mongoose.connect(url);
+        /*await client.connect();*/
+        /*await db.command({ping: 1});*/
+        if (mongoose.connection.readyState !== 1) {
+            throw new Error('Не встал =(')
+        }
         console.log('connected to db')
         return true
     } catch (err) {
         console.error(err);
-        await client.close();
+        await mongoose.disconnect();
         return false;
     }
 }
