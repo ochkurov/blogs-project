2a43da24e26fac3e79134481ee1ee0ff9bc4145d
chik1313 <ochkurov96@gmail.com>
2025-06-22 20:19:41 +0200

    make changes

--- src/posts/postsQwRepository.ts
+++ src/posts/postsQwRepository.ts
@@ -36,7 +36,7 @@ export class PostsQwRepository {
             .find(filter)
             .sort({[sortBy]: sortDirection === 'asc' ? 1 : -1})
             .skip((pageNumber - 1) * pageSize)
-            .limit(pageSize)s
+            .limit(pageSize)
             .lean()
         let postMap = new Map<string, LikeStatusEnum>()
 
